1e24c308c92fc77580d3c97c9f1dc6d024655a2f
Tasho0318 <146801635+Tasho0318@users.noreply.github.com>
2023-10-17 14:35:17 +0900

    Senna
    UPDATA

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,121 @@
 <template>
-    <h1>アイルトン・セナの戦績</h1>
-    <p>アイルトン・セナはブラジル出身の伝説的なフォーミュラ1ドライバーで、多くの栄誉を獲得しました。</p>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ayrton Senna</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
 
-    <h2>主な成績</h2>
-    <ul>
-        <li>世界選手権優勝回数: 3回（1988、1990、1991）</li>
-        <li>ポールポジション獲得回数: XX回</li>
-        <li>優勝回数: XX回</li>
-        <li>最速ラップ獲得回数: XX回</li>
-    </ul>
+        header {
+            background-color: #dc3545;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+        }
 
-    <h2>キャリアのハイライト</h2>
-    <p>アイルトン・セナのキャリアのハイライトを紹介します。詳細な戦績や出来事をここに追加できます。</p>
+        h1 {
+            margin: 0;
+            padding: 0;
+        }
 
-    <h2>フォーミュラ1チーム</h2>
-    <p>セナは以下のフォーミュラ1チームで競技しました。</p>
-    <ul>
-        <li>チーム1の名前</li>
-        <li>チーム2の名前</li>
-        <!-- 必要な分だけ続けて追加 -->
-    </ul>
+        .container {
+            max-width: 1080px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
 
-    <h2>画像</h2>
-    <img src="アイルトン・セナの画像のURL" alt="アイルトン・セナの画像" width="300">
+        h2 {
+            margin-top: 20px;
+        }
 
-    <h2>ビデオ</h2>
-    <iframe width="560" height="315" src="YouTubeなどのビデオの埋め込みコード" frameborder="0" allowfullscreen></iframe>
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            margin-bottom: 10px;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin: 20px 0;
+        }
+
+        iframe {
+            max-width: 100%;
+            height: 315px;
+        }
+
+        footer {
+            text-align: center;
+            background-color: #dc3545;
+            color: #fff;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>アイルトン・セナの戦績</h1>
+    </header>
+
+    <div class="container">
+        <img src="https://autotest.com.ar/wp-content/uploads/2021/04/AYRTON-SENNA.jpg" alt="https://autotest.com.ar/wp-content/uploads/2021/04/AYRTON-SENNA.jpg" style="max-width: 100%; height: auto;">
+        
+        <h2>プロフィール</h2>
+        <p>
+          アイルトン・セナ・ダ・シルバ（Ayrton Senna da Silva, 1960年3月21日 - 1994年5月1日）は、ブラジルのレーシング・ドライバー。F1世界選手権において、1988年・1990年・1991年と、計3度ワールドチャンピオンを獲得した。
+
+F1史を代表するドライバーの1人とされ、多くの投票で「史上最高のF1ドライバー」や「史上最も影響力のあるF1ドライバー」に選出されている。計65度のPP獲得数は、2006年にミハエル・シューマッハに更新されるまで歴代1位の記録であった。アラン・プロスト、ネルソン・ピケ、ナイジェル・マンセルとは、1980年代から1990年代前半のF1を象徴する存在として、「四強」「ビッグ4」「F1四天王」などと纏めて呼ばれることもある。特にプロストとの熾烈なライバル関係が知られ、日本では2人の争いが「セナ・プロ決戦」「セナ・プロ対決」などと呼ばれた。
+
+異名には「Genius（天才）」「マジック・セナ」などがあり、若手時代には「ハリー」の愛称でも呼ばれた。日本では、古舘伊知郎が名付けた「音速の貴公子」がよく知られている。
+        </p>
+
+        <h2>主な成績</h2>
+        <ul>
+            <li>世界選手権優勝回数: 3回（1988、1990、1991）</li>
+            <li>ポールポジション獲得回数: 65回</li>
+            <li>優勝回数: 41回</li>
+            <li>最速ラップ獲得回数: 19回</li>
+        </ul>
+
+        <h2>キャリアのハイライト</h2>
+        <p>
+            アイルトン・セナはそのキャリアで多くのハイライトを持ちました。1984年のモナコGPで初優勝を飾り、その後も数々のレースで成功を収めました。セナのフォーミュラ1キャリアは彼の驚異的なスピード、レースセンス、および情熱によって特徴づけられています。また、セナはモナコGPを6回制し、その記録は今日でも破られていません。
+        </p>
+
+        <h2>フォーミュラ1チーム</h2>
+        <p>
+            セナはフォーミュラ1で複数のチームで競技しました。その中には、トールマン、ロータス、マクラーレン、およびウィリアムズが含まれます。
+        </p>
+
+        <p>
+            アイルトン・セナがマクラーレンで乗っていたMP4/4は、16戦中15勝という伝説があります.
+        </p>
+
+        <img src="https://s2-ge.glbimg.com/Xtft6kSbv4nPg98wP3uZus7f9sQ=/0x0:5000x3333/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/3/B/SeQMo8R1AvC6AOQEEBsg/gettyimages-622342562.jpg" alt="https://s2-ge.glbimg.com/Xtft6kSbv4nPg98wP3uZus7f9sQ=/0x0:5000x3333/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/3/B/SeQMo8R1AvC6AOQEEBsg/gettyimages-622342562.jpg">
+        
+        <h2>ビデオ</h2>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/nL5liGMND_0?si=KNxGl24C2rxERaZ-&amp;start=553" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
 
     <footer>
         <p>&copy; 2023 アイルトン・セナのファンクラブ</p>
     </footer>
+</body>
+</html>
+
 </template>
