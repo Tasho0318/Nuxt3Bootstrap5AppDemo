bb81fc77aa39f2df7ca739543cd3eca96d9549bb
Tasho0318 <146801635+Tasho0318@users.noreply.github.com>
2023-11-07 13:22:38 +0900

    ページ
    追加

---
+++ pages/Driver/Schumacher.vue
@@ -0,0 +1,143 @@
+<template>
+    <!DOCTYPE html>
+    <html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Michael Schumacher</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f0f0f0;
+                margin: 0;
+                padding: 0;
+            }
+    
+            header {
+                background-color: #dc3545;
+                color: #fff;
+                text-align: center;
+                padding: 20px;
+            }
+    
+            h1 {
+                margin: 0;
+                padding: 0;
+            }
+    
+            .container {
+                max-width: 1080px;
+                margin: 20px auto;
+                padding: 20px;
+                background-color: #fff;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            }
+    
+            h2 {
+                margin-top: 20px;
+            }
+    
+            ul {
+                list-style-type: none;
+                padding: 0;
+            }
+    
+            li {
+                margin-bottom: 10px;
+            }
+    
+            img {
+                max-width: 100%;
+                height: auto;
+                display: block;
+                margin: 20px 0;
+            }
+    
+            iframe {
+                max-width: 100%;
+                height: 315px;
+            }
+    
+            footer {
+                text-align: center;
+                background-color: #dc3545;
+                color: #fff;
+                padding: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>Michael Schumacher</h1>
+        </header>
+    
+        <div class="container">
+            <img src="https://f1-gate.com/media/img2022/20220720-f1-michael-schumacher.jpg" alt="https://f1-gate.com/media/img2022/20220720-f1-michael-schumacher.jpg" style="max-width: 100%; height: auto;">
+            
+            <h2>プロフィール</h2>
+            <p>
+                ミハエル・シューマッハ（Michael Schumacher, 1969年1月3日 - ）は、ドイツの元レーシングドライバー。愛称はシューミ(Schumi)、マイケル(Michael)。日本語表記は他に「ミハエル・シューマッハー」やドイツ語の発音に近い「ミヒャエル・シューマッハ」もある。英語圏では「マイケル・シューマッカー」と発音される場合がある。
+
+ドイツ人初のF1ドライバーズチャンピオン。最多優勝91回、チャンピオン獲得7度などF1の主な個人記録を更新した。2006年に一度引退したが2010年に復帰し、2012年に再度現役を退いた。
+
+その正確なドライビングと、強靭なフィジカルから、日本では「ターミネーター」、日本国外では「サイボーグ」と呼ばれる時期があった。フェラーリ在籍期、フジテレビのF1中継では「皇帝」の愛称が使われた。
+
+6歳年下の実弟ラルフ・シューマッハはウィリアムズなどで6勝を挙げた元F1ドライバー。息子のミック・シューマッハもF1ドライバーであったが、2023年はシートを失いミハエルが最後に過ごしたメルセデスのリザーブドライバーになった。
+            </p>
+    
+            <h2>主な成績</h2>
+            <ul>
+                <li>世界選手権優勝回数: 7回（1994、1995、2000、2001、2002、2003、2004）</li>
+                <li>ポールポジション獲得回数: 68回</li>
+                <li>優勝回数: 91回</li>
+                <li>最速ラップ獲得回数: 77回</li>
+            </ul>
+    
+            <h2>キャリアのハイライト</h2>
+            <p>
+                ミハエル・シューマッハはそのキャリアで多くのハイライトを持ちました。彼の初のワールドチャンピオン獲得はベネトンでの時期であり、その後フェラーリで5度の連続優勝を果たしました。彼は、多くの瞬間で競技界を驚かせ、多くのファンから尊敬を受けています。
+            </p>
+    
+            <h2>フォーミュラ1チーム</h2>
+            <p>
+                シューマッハはフォーミュラ1で主にベネトンとフェラーリで競技しました。ベネトンでの成功後、フェラーリに移籍し、そこで数多くの栄光を勝ち取りました。
+            </p>
+    
+            <p>
+                シューマッハがフェラーリで乗っていたF2004は、その年の支配的なマシンとして知られ、多くのレースで圧倒的なパフォーマンスを見せました.
+            </p>
+    
+            <img src="https://s.response.jp/imgs/p/mNECeKegOUvqFa6-p0pU_X5Mz0HRQkNERUZH/35581.jpg" alt="https://s.response.jp/imgs/p/mNECeKegOUvqFa6-p0pU_X5Mz0HRQkNERUZH/35581.jpg">
+            
+            <h2>アイルトン・セナの最後</h2>
+            <p>
+                セナは1994年にウィリアムズへ移籍しました。このシーズンに乗っていたマシーンはWilliams FW16です。
+                Williamsのマシーンはハイテクデバイスが積まれていましたが、FIAのレギュレーションによりハイテクデバイスが制限されました。
+                そのためセナが操るFW16はとても運転が難しいものでした。
+            </p>
+    
+            <h2>セナが乗っていたWilliams FW16</h2>
+            <img src="https://cdn-image.as-web.jp/2021/04/29122833/asimg_P_1-11-c_29608a27e0a372e.jpg">
+            <p>
+                ですがF1 1994 第3戦 サンマリノGPで予選中にクラッシュで親友の「ローランド・ラッツェンバーガー」を失い、セナ本人も決勝レース中にクラッシュし搬送先の病院で亡くなりました。
+                セナは予選終了後、親友のラッツェンバーガーがなくなったことに対し「もう走りたくない」と恋人に電話をしていました。
+                事故の原因としては様々ありますが、最も有力な説だとステアリングをセナが好む大径のものに変えるためにステアリングシャフトを切断して再溶接したが強度不足だった、コクピットが狭く膝が当たるためステアリングシャフトの膝の部分を若干削った、などが原因でステアリングシャフトが折れてハンドルが全く効かなくなったという説です。
+                サイト下のビデオ動画00:09でセナはカウンターを当てましたがマシーンはいうことを聞かずに壁に衝突しました。
+                その際にマシーンの破片がセナのヘルメットを貫通し破片が脳に到達し、事故直後ではほぼ即死状態でした。
+            </p>
+            <img src="https://i0.wp.com/static.smalljoys.me/2018/01/the-fatal-accident-of-the-hero-ayrton-senna-in-the-f1-world_63168904.jpg?w=1308&ssl=1&strip=all">
+            <h2>ビデオ</h2>
+            <p>
+              ショッキングな内容が含まれます、ご視聴にはご注意下さい。
+            </p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/x9znf-lpg4Q?si=mDSzzCVgr-CVwmyz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    
+        <footer>
+            <p>&copy; 2023 アイルトン・セナ</p>
+        </footer>
+    </body>
+    </html>
+    
+    </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,141 +1,21 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="ja">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Ayrton Senna</title>
-      <style>
-          body {
-              font-family: Arial, sans-serif;
-              background-color: #f0f0f0;
-              margin: 0;
-              padding: 0;
-          }
-  
-          header {
-              background-color: #dc3545;
-              color: #fff;
-              text-align: center;
-              padding: 20px;
-          }
-  
-          h1 {
-              margin: 0;
-              padding: 0;
-          }
-  
-          .container {
-              max-width: 1080px;
-              margin: 20px auto;
-              padding: 20px;
-              background-color: #fff;
-              border-radius: 8px;
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-          }
-  
-          h2 {
-              margin-top: 20px;
-          }
-  
-          ul {
-              list-style-type: none;
-              padding: 0;
-          }
-  
-          li {
-              margin-bottom: 10px;
-          }
-  
-          img {
-              max-width: 100%;
-              height: auto;
-              display: block;
-              margin: 20px 0;
-          }
-  
-          iframe {
-              max-width: 100%;
-              height: 315px;
-          }
-  
-          footer {
-              text-align: center;
-              background-color: #dc3545;
-              color: #fff;
-              padding: 10px;
-          }
-      </style>
-  </head>
-  <body>
-      <header>
-          <h1>Ayrton Senna</h1>
-      </header>
-  
-      <div class="container">
-          <img src="https://autotest.com.ar/wp-content/uploads/2021/04/AYRTON-SENNA.jpg" alt="https://autotest.com.ar/wp-content/uploads/2021/04/AYRTON-SENNA.jpg" style="max-width: 100%; height: auto;">
-          
-          <h2>プロフィール</h2>
-          <p>
-            アイルトン・セナ・ダ・シルバ（Ayrton Senna da Silva, 1960年3月21日 - 1994年5月1日）は、ブラジルのレーシング・ドライバー。F1世界選手権において、1988年・1990年・1991年と、計3度ワールドチャンピオンを獲得した。
-  
-  F1史を代表するドライバーの1人とされ、多くの投票で「史上最高のF1ドライバー」や「史上最も影響力のあるF1ドライバー」に選出されている。計65度のPP獲得数は、2006年にミハエル・シューマッハに更新されるまで歴代1位の記録であった。アラン・プロスト、ネルソン・ピケ、ナイジェル・マンセルとは、1980年代から1990年代前半のF1を象徴する存在として、「四強」「ビッグ4」「F1四天王」などと纏めて呼ばれることもある。特にプロストとの熾烈なライバル関係が知られ、日本では2人の争いが「セナ・プロ決戦」「セナ・プロ対決」などと呼ばれた。
-  
-  異名には「Genius（天才）」「マジック・セナ」などがあり、若手時代には「ハリー」の愛称でも呼ばれた。日本では、古舘伊知郎が名付けた「音速の貴公子」がよく知られている。
-          </p>
-  
-          <h2>主な成績</h2>
-          <ul>
-              <li>世界選手権優勝回数: 3回（1988、1990、1991）</li>
-              <li>ポールポジション獲得回数: 65回</li>
-              <li>優勝回数: 41回</li>
-              <li>最速ラップ獲得回数: 19回</li>
-          </ul>
-  
-          <h2>キャリアのハイライト</h2>
-          <p>
-              アイルトン・セナはそのキャリアで多くのハイライトを持ちました。1984年のモナコGPで初優勝を飾り、その後も数々のレースで成功を収めました。セナのフォーミュラ1キャリアは彼の驚異的なスピード、レースセンス、および情熱によって特徴づけられています。また、セナはモナコGPを6回制し、その記録は今日でも破られていません。
-          </p>
-  
-          <h2>フォーミュラ1チーム</h2>
-          <p>
-              セナはフォーミュラ1で複数のチームで競技しました。その中には、トールマン、ロータス、マクラーレン、およびウィリアムズが含まれます。
-          </p>
-  
-          <p>
-              アイルトン・セナがマクラーレンで乗っていたMP4/4は、16戦中15勝という伝説があります.
-          </p>
-  
-          <img src="https://s2-ge.glbimg.com/Xtft6kSbv4nPg98wP3uZus7f9sQ=/0x0:5000x3333/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/3/B/SeQMo8R1AvC6AOQEEBsg/gettyimages-622342562.jpg" alt="https://s2-ge.glbimg.com/Xtft6kSbv4nPg98wP3uZus7f9sQ=/0x0:5000x3333/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/3/B/SeQMo8R1AvC6AOQEEBsg/gettyimages-622342562.jpg">
-          
-          <h2>アイルトン・セナの最後</h2>
-          <p>
-              セナは1994年にウィリアムズへ移籍しました。このシーズンに乗っていたマシーンはWilliams FW16です。
-              Williamsのマシーンはハイテクデバイスが積まれていましたが、FIAのレギュレーションによりハイテクデバイスが制限されました。
-              そのためセナが操るFW16はとても運転が難しいものでした。
-          </p>
-  
-          <h2>セナが乗っていたWilliams FW16</h2>
-          <img src="https://cdn-image.as-web.jp/2021/04/29122833/asimg_P_1-11-c_29608a27e0a372e.jpg">
-          <p>
-              ですがF1 1994 第3戦 サンマリノGPで予選中にクラッシュで親友の「ローランド・ラッツェンバーガー」を失い、セナ本人も決勝レース中にクラッシュし搬送先の病院で亡くなりました。
-              セナは予選終了後、親友のラッツェンバーガーがなくなったことに対し「もう走りたくない」と恋人に電話をしていました。
-              事故の原因としては様々ありますが、最も有力な説だとステアリングをセナが好む大径のものに変えるためにステアリングシャフトを切断して再溶接したが強度不足だった、コクピットが狭く膝が当たるためステアリングシャフトの膝の部分を若干削った、などが原因でステアリングシャフトが折れてハンドルが全く効かなくなったという説です。
-              サイト下のビデオ動画00:09でセナはカウンターを当てましたがマシーンはいうことを聞かずに壁に衝突しました。
-              その際にマシーンの破片がセナのヘルメットを貫通し破片が脳に到達し、事故直後ではほぼ即死状態でした。
-          </p>
-          <img src="https://i0.wp.com/static.smalljoys.me/2018/01/the-fatal-accident-of-the-hero-ayrton-senna-in-the-f1-world_63168904.jpg?w=1308&ssl=1&strip=all">
-          <h2>ビデオ</h2>
-          <p>
-            ショッキングな内容が含まれます、ご視聴にはご注意下さい。
-          </p>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/x9znf-lpg4Q?si=mDSzzCVgr-CVwmyz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      </div>
-  
-      <footer>
-          <p>&copy; 2023 アイルトン・セナ</p>
-      </footer>
-  </body>
-  </html>
-  
-  </template>
+    <div class="container">
+       <h1 class="page-title">F1で活躍した選手たち</h1>
+      <ul>
+        <li>
+         <nuxt-link to="/Driver/Senna">アイルトン・セナ</nuxt-link>
+        </li>
+        <li>
+         <nuxt-link to="/Driver/Schumacher">ミハエル・シューマッハ</nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <style>
+     
+      body {
+        background-color: #f5f5dc;
+      .page-title {
+      font-size: 48px;
+      margin-top: 40px;
+    </style>
+  </template>  
