ad76c98c362c356b780990615a310209aed0fd90
Tasho0318 <146801635+Tasho0318@users.noreply.github.com>
2023-11-28 14:10:06 +0900

    a
    a

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,82 @@
 <template>
-    <div class="container">
-       <h1 class="page-title">F1で活躍した選手たち</h1>
-      <ul>
-        <li>
-         <nuxt-link to="/Driver/Senna">アイルトン・セナ</nuxt-link>
-        </li>
-        <li>
-         <nuxt-link to="/Driver/Schumacher">ミハエル・シューマッハ</nuxt-link>
-        </li>
-      </ul>
-    </div>
-    <style>
+  <!DOCTYPE html>
+  <html lang="ja">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Ayrton Senna</title>
+      <style>
+          body {
+              font-family: Arial, sans-serif;
+              background-color: #f0f0f0;
+              margin: 0;
+              padding: 0;
+          }
+  
+          header {
+              background-color: #dc3545;
+              color: #fff;
+              text-align: center;
+              padding: 20px;
+          }
+  
+          h1 {
+              margin: 0;
+              padding: 0;
+          }
+  
+          .container {
+              max-width: 1080px;
+              margin: 20px auto;
+              padding: 20px;
+              background-color: #fff;
+              border-radius: 8px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+          }
+  
+          h2 {
+              margin-top: 20px;
+          }
+  
+          ul {
+              list-style-type: none;
+              padding: 0;
+          }
+  
+          li {
+              margin-bottom: 10px;
+          }
+  
+          img {
+              max-width: 100%;
+              height: auto;
+              display: block;
+              margin: 20px 0;
+          }
+  
+          iframe {
+              max-width: 100%;
+              height: 315px;
+          }
+  
+          footer {
+              text-align: center;
+              background-color: #dc3545;
+              color: #fff;
+              padding: 10px;
+          }
+      </style>
+  </head>
+  <body>
+      <header>
+          <h1>Formula One</h1>
+      </header>
+  
      
-      body {
-        background-color: #f5f5dc;
-      .page-title 
-      font-size: 48px;
-      margin-top: 40px;
-      }
-    </style>
-  </template>  
+      <footer>
+          <p>&copy; 2023 Formula One Champion Ship</p>
+      </footer>
+  </body>
+  </html>
+  
+  </template>
