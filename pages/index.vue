13d28f382ae0a0c0733897dd65db3abe9a9c59ed
Tasho0318 <146801635+Tasho0318@users.noreply.github.com>
2023-12-31 17:40:58 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,28 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="ja">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Ayrton Senna</title>
-      <style>
-          body {
-              font-family: Arial, sans-serif;
-              background-color: #f0f0f0;
-              margin: 0;
-              padding: 0;
-          }
-  
-          header {
-              background-color: #dc3545;
-              color: #fff;
-              text-align: center;
-              padding: 20px;
-          }
-  
-          h1 {
-              margin: 0;
-              padding: 0;
-          }
-  
-          .container {
-              max-width: 1080px;
-              margin: 20px auto;
-              padding: 20px;
-              background-color: #fff;
-              border-radius: 8px;
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-          }
-  
-          h2 {
-              margin-top: 20px;
-          }
-  
-          ul {
-              list-style-type: none;
-              padding: 0;
-          }
-  
-          li {
-              margin-bottom: 10px;
-          }
-  
-          img {
-              max-width: 100%;
-              height: auto;
-              display: block;
-              margin: 20px 0;
-          }
-  
-          iframe {
-              max-width: 100%;
-              height: 315px;
-          }
-  
-          footer {
-              text-align: center;
-              background-color: #dc3545;
-              color: #fff;
-              padding: 10px;
-          }
-      </style>
-  </head>
-  <body>
-      <header>
-          <h1>Formula One</h1>
-      </header>
-  
-     
-      <footer>
-          <p>&copy; 2023 Formula One Champion Ship</p>
-      </footer>
-  </body>
-  </html>
-  
+    <div>
+      <h1>Red Bull サイト</h1>
+      <div v-if="redbullInfo">
+        <h2>{{ redbullInfo.name }}</h2>
+        <p>{{ redbullInfo.description }}</p>
+        <img :src="redbullInfo.image" alt="Red Bull">
+      </div>
+      <div v-else>
+        <p>データを取得中...</p>
+      </div>
+    </div>
   </template>
+  
+  <script>
+  export default {
+    async asyncData({ $axios }) {
+      try {
+        const response = await $axios.get('https://api.example.com/redbull'); // Red BullのAPIエンドポイントを実際のものに変更してください
+        return { redbullInfo: response.data };
+      } catch (error) {
+        console.error('APIエラー:', error);
+        return { redbullInfo: null };
+      }
+    },
+  };
+  </script>
+  
